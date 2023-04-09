886d81ab160d670d13b09409b42af4ac507dfd76
minhthang1410 <minhthang14101998@gmail.com>
2023-04-09 11:38:18 +0700

    update login-register

--- login-register/login.js
+++ login-register/login.js
@@ -15,9 +15,9 @@ form.addEventListener("submit", (e) => {
         },
         body: JSON.stringify(body),
     }).then((response) => response.json()).then((data) => {
-        localStorage.setItem("auth_token", data.auth_token);
         if (data.status === "success") {
-            window.location = "index.html";   
+            window.location = "index.html";
+            localStorage.setItem("auth_token", data.auth_token);
         } else {
             alert(data.message);
         }
